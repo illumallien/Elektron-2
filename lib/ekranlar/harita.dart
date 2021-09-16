dd6e0414738b0fa573b8644bf944e4a2cbf69a9f
illumallien <rabiaguneyli@gmail.com>
2021-09-16 18:41:14 +0300

    marker

--- lib/ekranlar/harita.dart
+++ lib/ekranlar/harita.dart
@@ -11,7 +11,9 @@ class HaritaSayfasi extends StatefulWidget {
 
 class _HaritaSayfasiState extends State<HaritaSayfasi> {
   Set<Marker> _markers = {};
-  var mapMarker;
+  var tarihiMarker;
+  var camimarker;
+  var trenmarker;
 
   @override
   void initState() {
@@ -20,21 +22,16 @@ class _HaritaSayfasiState extends State<HaritaSayfasi> {
   }
 
   Future<void> setCustomMarker() async {
-    mapMarker = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(), "assets/markers/tarihi_marker.png");
+    tarihiMarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(10, 10)),
+        "assets/markers/tarihi_marker.png");
+
+    camimarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(10, 10)), "assets/markers/mosque.png");
+
+    trenmarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(10, 10)), "assets/markers/train.png");
   }
-
-/*
-
-Marker(
-        markerId: MarkerId("Id-3"),
-        position: LatLng(41.00859070323684, 28.980172176274984),
-        infoWindow: InfoWindow(
-          title: "The Hagia Sophia Mosque",
-        ),
-        visible: true),
-
-*/
 
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
@@ -42,29 +39,24 @@ Marker(
         Marker(
             markerId: MarkerId("Id-1"),
             position: LatLng(41.02569414363595, 28.974166688314174),
-            icon: mapMarker,
+            icon: tarihiMarker,
             infoWindow: InfoWindow(
               title: "Galata Tower",
             ),
             visible: true),
-      ); /*
+      );
       _markers.add(Marker(
           markerId: MarkerId("Id-2"),
           position: LatLng(41.00540250566176, 28.97681618188754),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: camimarker,
           infoWindow: InfoWindow(
             title: "The Blue Mosque",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-3"),
           position: LatLng(41.00859070323684, 28.980172176274984),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: camimarker,
           infoWindow: InfoWindow(
             title: "The Hagia Sophia Mosque",
           ),
@@ -72,101 +64,75 @@ Marker(
       _markers.add(Marker(
           markerId: MarkerId("Id-4"),
           position: LatLng(41.011527777848976, 28.98337847003785),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Topkapı Palace",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-5"),
           position: LatLng(41.010689655546386, 28.968067792686316),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Grand Bazaar",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-6"),
           position: LatLng(41.00836267655452, 28.977839908103757),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Basilica Cistern",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-7"),
           position: LatLng(41.03378328974789, 28.97793533842894),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Istiklal Street",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-8"),
           position: LatLng(41.08481909539014, 29.056695817856847),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Rumeli Fortress",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-9"),
           position: LatLng(41.08593296036114, 29.07072135440755),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Anatolian Fortress",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-10"),
           position: LatLng(40.99385959578696, 28.923269417022766),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Yedikule Dungeons",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-11"),
           position: LatLng(40.99682677560662, 29.01929413252782),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: trenmarker,
           infoWindow: InfoWindow(
             title: "Haydarpasa Train Station",
           ),
           visible: true));
-
       _markers.add(Marker(
           markerId: MarkerId("Id-12"),
           position: LatLng(41.0391644201605, 29.00045765709285),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration(size: Size(10, 10)),
-              'assets/markers/tarihi_marker.png'),
+          icon: tarihiMarker,
           infoWindow: InfoWindow(
             title: "Dolmabahçe Palace",
           ),
-          visible: true));*/
+          visible: true));
     });
   }
 
